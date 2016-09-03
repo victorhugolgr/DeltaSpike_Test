20b76a9e201b0b494805fb928ecf97e60ec5eac9
vandame <vandame@vandame-Inspiron-5537>
2016-09-03 12:46:25 -0300

    Tela de cadastro de cliente.

---
+++ delta-spike-test/src/main/java/br/com/victorhugolgr/controller/ClienteListMB.java
@@ -0,0 +1,48 @@
+package br.com.victorhugolgr.controller;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.victorhugolgr.model.Cliente;
+import br.com.victorhugolgr.repository.ClienteRepository;
+
+@Named
+@ViewScoped
+public class ClienteListMB implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Cliente cliente;
+
+	@Inject
+	private ClienteRepository service;
+	
+	@PostConstruct
+	public void init(){
+		cliente = new Cliente();
+	}
+
+	public void salvar() {
+		cliente = service.save(cliente);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Salvo com sucesso! ID = " + cliente.getId(), "INFO"));
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
